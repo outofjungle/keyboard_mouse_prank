cab924dfff69f84230caa301fe67c4e39c9e62ab
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2014-03-31 20:53:28 -0700

    mostly final

--- keyboard_mouse_prank.ino
+++ keyboard_mouse_prank.ino
@@ -1,63 +1,44 @@
-
-const byte pin2 = 2;
-const byte pin3 = 3;
-
-int pin2_old_state = HIGH;
-int pin3_old_state = HIGH;
+#define MIN_DELAY 360
+#define MAX_DELAY 720
 
 int times;
 int counter;
 int choice;
 
 void setup() {
-  pinMode(pin2, INPUT);
-  pinMode(pin3, INPUT);
   Keyboard.begin();
   Mouse.begin();
+  counter = MIN_DELAY;
 }
 
 void loop() {
-  int pin2_state = digitalRead(pin2);
-  int pin3_state = digitalRead(pin3);
-
-  times = random(1, 6);
-
-  if ((pin2_state != pin2_old_state) && (pin2_state == HIGH)) {
-    move_mouse(times);
-    delay(30);
-  }
-  else if ((pin3_state != pin3_old_state) && (pin3_state == HIGH)) {
-    send_cmd_tab(times);
-  }
-  else if ( counter <= 0 ) {
-    choice = random(2);
+  if ( counter <= 0 ) {
+    times = random(2, 6);
     
+    choice = random(2);    
     if (choice) {
       move_mouse(times);
     } else {
       send_cmd_tab(times);
     }
-    counter = random(3, 6);
+    counter = random(MIN_DELAY, MAX_DELAY);
   }
   else {
     counter--;
     delay(1000);
   }  
-  
-  pin2_old_state = pin2_state; 
-  pin3_old_state = pin3_state;
 }
 
-
 void move_mouse(int times) {
-  for(int i=0; i < times; i++) {  
+  for(int i=0; i < times; i++) {
+
     int r = random(10, 20);
     float t = (float) random(6);
-    
     float dir = random(2) ? -1.0 : 1.0;
+    
     for(int itr = 0; itr < 30; itr++) {
-      t = t + (dir * 0.1);
       
+      t = t + (dir * 0.1);
       int move_x = r * cos(t);
       int move_y = r * sin(t);
       
@@ -68,11 +49,14 @@ void move_mouse(int times) {
 }
 
 void send_cmd_tab(int times) {
+
   Keyboard.press(KEY_LEFT_GUI);
   delay(30);
+
   for(int i=0; i < times; i++) {
     Keyboard.press(KEY_TAB);
     delay(30);
+
     Keyboard.release(KEY_TAB);
     delay(30);
   }
