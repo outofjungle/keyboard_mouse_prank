3df4f810a514c86ded511a2df807423daca3e4dc
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2014-03-31 20:26:23 -0700

    prank with buttons

--- keyboard_mouse_prank.ino
+++ keyboard_mouse_prank.ino
@@ -1,25 +1,65 @@
 
-const int up_button = 2;     
-const int down_button = 3;        
+const int pin2 = 2;
+const int pin3 = 3;
 
-int range = 5;
-int response_delay = 10;
+int pin2_old_state = HIGH;
+int pin3_old_state = HIGH;
 
+int times;
 void setup() {
-  pinMode(up_button, INPUT);
-  pinMode(down_button, INPUT);
+  pinMode(pin2, INPUT);
+  pinMode(pin3, INPUT);
+  Keyboard.begin();
   Mouse.begin();
 }
 
 void loop() {
-  int up_state = digitalRead(up_button);
-  int down_state = digitalRead(down_button);
+  int pin2_state = digitalRead(pin2);
+  int pin3_state = digitalRead(pin3);
 
-  int  distance = (up_state - down_state)*range;
+  times = 1 + rand() % 5;
 
-  if (distance != 0) {
-    Mouse.move(0, distance, 0);
+  if ((pin2_state != pin2_old_state) && (pin2_state == HIGH)) {
+    move_mouse(times);
+    delay(30);
   }
+  
+  if ((pin3_state != pin3_old_state) && (pin3_state == HIGH)) {
+    send_cmd_tab(times);
+  }
+  
+  pin2_old_state = pin2_state; 
+  pin3_old_state = pin3_state;
+}
 
-  delay(response_delay);
+
+void move_mouse(int times) {
+  for(int i=0; i < times; i++) {  
+    int r = 10 + rand() % 20;
+    float t = (float) ( rand() % 6 );
+    
+    float dir = (rand() % 1) ? -1.0 : 1.0;
+    for(int itr = 0; itr < 30; itr++) {
+      t = t + (dir * 0.1);
+      
+      int move_x = r * cos(t);
+      int move_y = r * sin(t);
+      
+      Mouse.move(move_x, move_y, 0);
+      delay(2);
+    }
+  }
+}
+
+void send_cmd_tab(int times) {
+  Keyboard.press(KEY_LEFT_GUI);
+  delay(30);
+  for(int i=0; i < times; i++) {
+    Keyboard.press(KEY_TAB);
+    delay(30);
+    Keyboard.release(KEY_TAB);
+    delay(30);
+  }
+  Keyboard.release(KEY_LEFT_GUI);
+  delay(30);
 }
